f18e6bfa12d69a08f815893c3bc7ed5480174b84
Allen Hsieh <allenhsiehhr@gmail.com>
2018-10-11 17:36:45 -0700

    Added a counter for number of search results and selected items

--- react-client/src/components/SearchResults.jsx
+++ react-client/src/components/SearchResults.jsx
@@ -6,6 +6,9 @@ const SearchResults = ({searchResults, handleCheck, handleSelectAll}) => {
   let selectAll;
   (searchResults.length > 0) && (selectAll = <SelectAll handleSelectAll={handleSelectAll}/>);
   return (<div>
+    <br />
+    Search found <b>{searchResults && searchResults.length}</b> items<br/>
+    Selected items: <b>{searchResults && searchResults.filter(item => item.checked).length}</b>
     {selectAll}
     <form>
       {searchResults.map((item, index) => {
--- react-client/src/components/SelectAll.jsx
+++ react-client/src/components/SelectAll.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const SelectAll = ({handleSelectAll}) => (
   <form>
+    <br />
     <label>
       <input
         name="selectAll"
